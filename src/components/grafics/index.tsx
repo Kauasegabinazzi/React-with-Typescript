b3d716b7639dbc5a7f3be48756064e2ccd3d1f08
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-08-08 00:03:00 -0300

    13-commit

--- src/components/grafics/index.tsx
+++ src/components/grafics/index.tsx
@@ -2,6 +2,7 @@ import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
 import Iconsulta from "../../types/Iconsulta";
 import Iproficional from "../../types/Iproficional";
 import useDadosGrafico from "./useDadosGrafics";
+import styled from "styled-components";
 
 interface Props {
     profissionais: Iproficional[] | null,
@@ -9,21 +10,28 @@ interface Props {
 }
 
 
-interface Idados{
-    nome:string,
+interface Idados {
+    nome: string,
     consultas: number
 }
 
+const SectionStyled = styled.section`
+    background-color: var(--branco);
+    border-radius: 16px;
+`
+
 function Grafico({ profissionais, consultas }: Props) {
-    let dados:  Array<Idados> =  useDadosGrafico({profissionais, consultas})
+    let dados: Array<Idados> = useDadosGrafico({ profissionais, consultas })
     return (
-        <ResponsiveContainer width="100%" height={350}>
-            <BarChart layout="vertical" data={dados} margin={{top:15, right:40, left:40, bottom:20}}>
-                <XAxis type="number"></XAxis>
-                <YAxis type="category" dataKey="name"></YAxis>
-                <Bar dataKey="consultas" fill="#083860" barSize={30}></Bar>
-            </BarChart>
-        </ResponsiveContainer>
+        <SectionStyled>
+            <ResponsiveContainer width="100%" height={350}>
+                <BarChart layout="vertical" data={dados} margin={{ top: 15, right: 40, left: 40, bottom: 20 }}>
+                    <XAxis type="number"></XAxis>
+                    <YAxis type="category" dataKey="name"></YAxis>
+                    <Bar dataKey="consultas" fill="#083860" barSize={30}></Bar>
+                </BarChart>
+            </ResponsiveContainer>
+        </SectionStyled>
     )
 }
 
